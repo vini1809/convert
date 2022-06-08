625cde66e87d927996af97b144b8f226784a6e50
Vinicius Oruam <violetoru@ymail.com>
2022-06-08 07:39:26 -0300

    Painel de conversão monetario layout okay !

--- Conversor/src/dinheiro/conversorMonetario.java
+++ Conversor/src/dinheiro/conversorMonetario.java
@@ -4,9 +4,6 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Toolkit;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -16,6 +13,8 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class conversorMonetario extends JFrame {
 
@@ -50,9 +49,10 @@ public class conversorMonetario extends JFrame {
 		setJMenuBar(menuBar);
 		
 		JButton btnNewButton = new JButton("Dinheiro");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dinheiroConverter obj = new dinheiroConverter();
+				obj.setVisible(true);
 			}
 		});
 		btnNewButton.setBackground(new Color(245, 255, 250));
@@ -72,80 +72,5 @@ public class conversorMonetario extends JFrame {
 		txtBemVindoAo.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 14));
 		menuBar.add(txtBemVindoAo);
 		txtBemVindoAo.setColumns(20);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(238, 130, 238));
-		contentPane.setBorder(UIManager.getBorder("InternalFrame.border"));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Tipo de moeda");
-		lblNewLabel.setToolTipText("Tipo de moeda");
-		lblNewLabel.setFont(new Font("Times New Roman", Font.ITALIC, 32));
-		lblNewLabel.setBounds(70, 45, 200, 30);
-		contentPane.add(lblNewLabel);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(294, 45, 52, 36);
-		contentPane.add(comboBox);
-		
-		JLabel lblValorASer = new JLabel("Valor a ser convertido");
-		lblValorASer.setToolTipText("Valor a ser convertido");
-		lblValorASer.setFont(new Font("Times New Roman", Font.ITALIC, 32));
-		lblValorASer.setBounds(70, 126, 298, 30);
-		contentPane.add(lblValorASer);
-		
-		textField = new JTextField();
-		textField.setBounds(381, 126, 96, 31);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblParaQualMoeda = new JLabel("Para qual moeda voc\u00EA quer converter ?");
-		lblParaQualMoeda.setToolTipText("Para qual moeda voc\u00EA quer converter ?");
-		lblParaQualMoeda.setFont(new Font("Times New Roman", Font.ITALIC, 32));
-		lblParaQualMoeda.setBounds(70, 205, 518, 30);
-		contentPane.add(lblParaQualMoeda);
-		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Reais");
-		rdbtnNewRadioButton.setForeground(new Color(0, 0, 0));
-		rdbtnNewRadioButton.setBackground(new Color(255, 0, 255));
-		rdbtnNewRadioButton.setFont(new Font("Times New Roman", Font.ITALIC, 18));
-		rdbtnNewRadioButton.setBounds(70, 255, 71, 21);
-		contentPane.add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnDolares = new JRadioButton("Dolares");
-		rdbtnDolares.setBackground(new Color(255, 0, 255));
-		rdbtnDolares.setFont(new Font("Times New Roman", Font.ITALIC, 18));
-		rdbtnDolares.setBounds(143, 255, 87, 21);
-		contentPane.add(rdbtnDolares);
-		
-		JRadioButton rdbtnEuros = new JRadioButton("Euros");
-		rdbtnEuros.setBackground(new Color(255, 0, 255));
-		rdbtnEuros.setFont(new Font("Times New Roman", Font.ITALIC, 18));
-		rdbtnEuros.setBounds(232, 255, 71, 21);
-		contentPane.add(rdbtnEuros);
-		
-		JRadioButton rdbtnYans = new JRadioButton("Ienes");
-		rdbtnYans.setBackground(new Color(255, 0, 255));
-		rdbtnYans.setFont(new Font("Times New Roman", Font.ITALIC, 18));
-		rdbtnYans.setBounds(305, 255, 65, 21);
-		contentPane.add(rdbtnYans);
-		
-		JRadioButton rdbtnRenmimbi = new JRadioButton("Renmimbi");
-		rdbtnRenmimbi.setBackground(new Color(255, 0, 255));
-		rdbtnRenmimbi.setFont(new Font("Times New Roman", Font.ITALIC, 18));
-		rdbtnRenmimbi.setBounds(372, 255, 105, 21);
-		contentPane.add(rdbtnRenmimbi);
-		
-		JLabel lblOResultadoObtido = new JLabel("O Resultado obtido na convers\u00E3o \u00E9: ");
-		lblOResultadoObtido.setToolTipText("O Resultado obtido na convers\u00E3o \u00E9: ");
-		lblOResultadoObtido.setFont(new Font("Times New Roman", Font.ITALIC, 32));
-		lblOResultadoObtido.setBounds(70, 317, 470, 30);
-		contentPane.add(lblOResultadoObtido);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(543, 317, 105, 30);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
 	}
 }
